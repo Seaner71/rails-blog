8bac97df5778931e6233f5f9d39f8b72ee940055
echo 'hello world <ssmyth1071@gmail.com>
2017-11-20 21:29:13 -0500

    update users_show routes and attempt welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
-  def new
-    @user = User.new
+  def create
+      @user = User.find_or_create_by(username: username)
+      # session[:user_id] = user.id
+      if @user.save
+        redirect_to user
+      else
+        redirect_to welcome_index
+    end
   end
 end
