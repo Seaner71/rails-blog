6f8b060d9d1f044142a31acdc21dab2cfbf79bbe
echo 'hello world <ssmyth1071@gmail.com>
2017-11-23 13:46:20 -0500

    update methods on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new
+    @post = Post.find_by_id(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
   end
+
   def destroy
+    @post = Post.find_by_id(params[:post_id]
+    @comment = @post.comments.find_by_id(params[:id])
     @comment.destroy
     redirect_to posts_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
